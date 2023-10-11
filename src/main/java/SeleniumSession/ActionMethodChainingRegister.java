df3d4fed29a9d6838a8dfe47a341bcd548607018
Mreenal Singh <mreenal.singh@hotmail.com>
2023-10-11 14:53:21 +0200

    test of action method

--- src/main/java/SeleniumSession/ActionMethodChainingRegister.java
+++ src/main/java/SeleniumSession/ActionMethodChainingRegister.java
@@ -1,9 +1,14 @@
 package SeleniumSession;
 
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.remote.RemoteWebDriver;
+
 public class ActionMethodChainingRegister {
 
 	public static void main(String[] args) {
 		// This change is for testing pull request
+		
+		//WebDriver driver = new RemoteWebDriver(null, null);
 
 	}
 
